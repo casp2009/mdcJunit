3f82a98a709b5aabed05184859070e0fef34a156
casp2009 <56613238+casp2009@users.noreply.github.com>
2020-11-23 22:32:57 -0300

    Add files via upload

--- src/main/java/com/mycompany/java/mdc/MathUtil.java
+++ src/main/java/com/mycompany/java/mdc/MathUtil.java
@@ -6,9 +6,31 @@ package com.mycompany.java.mdc;
  */
 public class MathUtil {
     public static int mdc(int a, int b){
+        
+        //propriedade 7
+        a = Math.abs(a);
+        b = Math.abs(b);
+       
+        //propriedade 6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+        
+        //propriedade 1
         if(b > 0 && a % b == 0){
             return b;
         }
+        
+        //propriedade 3
+        if(b == 0){
+            return Math.abs(a);
+        }
+        
+        //propriedade 5
+        if(a % b != 0){
+            return 1;
+        }
+        
         return -1;
     }
 }
